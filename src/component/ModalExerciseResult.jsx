81f3c1de83a0fd6ef3ccd66a38337a28302d7cd6
sihyeon <121538822+oesikunare@users.noreply.github.com>
2024-06-04 14:20:20 +0900

    Add files via upload

---
+++ src/component/ModalExerciseResult.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ExerciseResult.css';
+
+const Modal = ({ show, onClose, children }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-button" onClick={onClose}>
+          &times;
+        </button>
+        <div className="modal-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
